002e3d10173ac4e1562beb49d80698073b6b04f5
yixun-h <61463722+yixun-h@users.noreply.github.com>
2020-10-29 14:24:44 +0100

    Update and rename count.py to inference/count.py

--- inference/count.py
+++ inference/count.py
@@ -1,11 +1,15 @@
 #!/Users/apple/anaconda3/bin/python3
 #!/usr/bin/env python3
-# this script is used to sum frequency of the same sequences with different lengths
-# and keep the shortest one.
-# ./count.py IGHV4-4_07.fasta sum_IGHV4-4_07.fasta
-# cd fasta/
-# for f in *; do ./../count.py $f ../2_sum_80/$f; done
-#
+'''
+Title: count.py
+Author:Yixun Huang
+Description:
+    This program will sum frequency of the same sequences with different lengths and keep the best one.
+    The longest sequence which frequency (sum with the ones longer than it) is more than 80% of total was considered as the best one.
+Usage:
+    python count.py IGHV4-4_07.fasta sum_IGHV4-4_07.fasta
+'''
+
 import sys
 count=[]
 seq_list=[]
@@ -23,22 +27,16 @@ with open (sys.argv[1],'r') as fin, open (sys.argv[2],'w') as fout:
             seq_list.append(line[::-1])
     seq_dict=dict(zip(seq_list,count))
     seq_list.sort(key=len) # sorts by Ascending length
-
-    #print(seq_list)
-    #print(seq_dict)
-    #for i in range(len(seq_list)):
     for seq in seq_list:
         counter=0
         for seq2 in seq_list:
             if seq2.startswith(seq):
                 counter+=seq_dict[seq2]
                 seqc_dict[seq]=counter
-    #print(seqc_dict)
     for i in range(len(seq_list)):
         for j in range(len(seq_list)):
             if i !=j and seq_list[i].startswith(seq_list[j]):
                 seq_remove.append(seq_list[i])
-
     for seq in seq_list:
         if not seq in seq_remove:
             seq_short.append(seq)
@@ -55,12 +53,3 @@ with open (sys.argv[1],'r') as fin, open (sys.argv[2],'w') as fout:
             if seqc_dict[seq_final[i][j]] > seqc_dict[seq_final[i][-1]] * 80/100:
                 print('>{}\n{}'.format(seqc_dict[seq_final[i][-1]],seq_final[i][j][::-1]),file=fout)
                 break
-'''
-if len(seq_list)>1 and len(seq_list[0])+10<len(seq_list[1]):
-    del seq_dict[seq_list[0]]
-    seq_list.pop(0)
-        #print(seqc_dict[seq_final[i][-1]])
-###
-#!the final count should consider the ones shorter than itself.
-###
-'''
