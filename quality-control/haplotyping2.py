dbe0ffca4925ac6022918d6b628c995124689069
yixun-h <61463722+yixun-h@users.noreply.github.com>
2021-06-01 13:51:17 +0800

    Update haplotyping2.py

--- quality-control/haplotyping2.py
+++ quality-control/haplotyping2.py
@@ -1,11 +1,12 @@
 #!/Users/apple/anaconda3/bin/python3
 #!/usr/bin/env python
-# If you have two different leader/5'UTR sequences for one specific allele,
-# you could support their existence by showing that one of the sequences are only found on one chromosome
+# If you have two different IGHV allele in the same subject,
+# their existence can be supported by showing that 5'UTR-leader sequences of one of allele are only found on one chromosome
 # while the other is only found on the other allele.
 
-# descripting: this script is used to do the hapltyping for one allele from a individual
-#  ./haplotyping.py IGHV4-30-2*01 IGHV4-30-2_01
+# descripting: this script is used to do the hapltyping for one allele from a individual by counting the number of 5'UTR-leader sequence
+# associated with IGHJ6*02 or IGHV6*03
+# python  ~/code/haplotyping2.py IGHV4-30-2*01 splitting/output/IGHV4-30-2_01
 import pandas as pd
 import sys
 df = pd.read_csv("3_merged.tab", sep = '\t')
